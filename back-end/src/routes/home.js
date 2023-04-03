2dd5aaf5235d5fb869efb4aca7e6f2456b2a5365
VitorKreis <101025640+VitorKreis@users.noreply.github.com>
2023-04-03 17:49:50 -0300

    commit

--- back-end/src/routes/home.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import HomeController from '../controller/HomeController.js';
-
-const router = new Router();
-
-router.get('/', HomeController.home);
-
-export default router;
--- back-end/src/server.js
+++
@@ -1,6 +0,0 @@
-import app from './app.js';
-
-const PORT = 5101;
-app.listen(PORT, () => {
-  console.log('Server rodando');
-});
