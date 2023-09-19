5516c57b2a88d58cfe8e422382268c26015543aa
VitorKreis <101025640+VitorKreis@users.noreply.github.com>
2023-09-19 16:42:11 -0300

    Delete back-end directory

--- back-end/src/routes/homeRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import HomeController from '../controller/HomeController';
-
-const router = Router();
-
-router.get('/', HomeController.index);
-
-export default router;
--- back-end/src/server.js
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-const PORT = 3110;
-
-app.listen(PORT, () => {
-  console.log('API Rodando!');
-  console.log('http://localhost:3110');
-});
